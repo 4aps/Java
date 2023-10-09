f0dcf6337d787113ef8371ecc61a1a6d592e8520
Akhand Pratap <132538768+4aps@users.noreply.github.com>
2023-10-09 23:51:12 +0530

    Create duplicate.java

---
+++ Java_Basics/duplicate.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class duplicate {
+    static int repeatedNum(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    return arr[i];
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements you want to input: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter the elements of array: ");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The repeated number is: "+ repeatedNum(arr));
+    }
+}
