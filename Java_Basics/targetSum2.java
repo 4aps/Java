482148ea3dfce483433d2901184002afdbf7d87f
Akhand Pratap <132538768+4aps@users.noreply.github.com>
2023-10-09 23:40:11 +0530

    Create targetSum2.java

---
+++ Java_Basics/targetSum2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class targetSum2 {
+    static int tripletSum(int arr[], int target){
+        int n = arr.length;
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j ++){
+                for(int k = j + 1; k < n; k++){
+                    if(arr[i]  + arr[j] + arr[k] == target){
+                        ans++;
+                }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements you want to input: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter the elements of array: ");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter the sum you need: ");
+        int target = sc.nextInt();;
+        System.out.println("This tripletSum is called " + tripletSum(arr, target) + " times.");
+    }
+}
