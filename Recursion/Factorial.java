668bace936ff59547e402d4f54877d04d1ab868b
Akhand Pratap <132538768+4aps@users.noreply.github.com>
2025-08-05 08:08:41 +0530

    Create Factorial.java
    factorial using recursion

---
+++ Recursion/Factorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Factorial {
+    static int fact(int num){
+        if(num <= 1)
+            return 1;
+        else
+            return num * fact(num-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int ans = fact(num);
+
+        System.out.println(ans);
+        sc.close();
+    }
+}
